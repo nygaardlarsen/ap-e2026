18b19d51c1c0f0fb2185d38e8c4d6e536633956a
nygaardlarsen <mathias.nygaard.iversen@gmail.com>
2026-09-10 11:12:24 +0200

    first changes to a1

--- week1/handout/src/APL/Eval.hs
+++ week1/handout/src/APL/Eval.hs
@@ -1,6 +1,64 @@
 module APL.Eval
   (
+    Val(..), eval
   )
 where
 
-import APL.AST ()
+import APL.AST (Exp(..))
+
+type Error = String
+
+data Val
+  = ValInt Integer
+  | ValBool Bool
+  deriving (Eq, Show)
+
+eval :: Exp -> Either Error Val
+eval e = case e of
+  (CstInt x) -> Right (ValInt x)
+  Add e1 e2 -> 
+    case (eval e1, eval e2) of 
+      (Left err, _) -> Left err
+      (_, Left err) -> Left err
+      (Right (ValInt x), Right (ValInt y)) -> Right (ValInt (x + y))
+  Sub e1 e2 ->
+    case (eval e1, eval e2) of 
+      (Left err, _) -> Left err
+      (_, Left err) -> Left err
+      (Right (ValInt x), Right (ValInt y)) -> Right (ValInt (x - y))
+  Mul e1 e2 ->
+    case (eval e1, eval e2) of 
+      (Left err, _) -> Left err
+      (_, Left err) -> Left err
+      (Right (ValInt x), Right (ValInt y)) -> Right (ValInt (x * y))
+  Div e1 e2 ->
+    case (eval e1, eval e2) of 
+      (Left err, _) -> Left err
+      (_, Left err) -> Left err
+      (_, Right (ValInt 0)) -> Left "Error: division by zero"
+      (Right (ValInt x), Right (ValInt y)) -> Right (ValInt (div x y))
+  Pow e1 e2 -> 
+    case (eval e1, eval e2) of 
+      (Left err, _) -> Left err
+      (_, Left err) -> Left err
+      (Right (ValInt x), Right (ValInt y)) -> 
+        if y < 0 then Left "Error: negative exponent"
+        else Right (ValInt (x ^ y))
+  (CstBool b) -> Right (ValBool b)
+  Eql e1 e2 ->
+    case (eval e1, eval e2) of
+      (Left err, _) -> Left err
+      (_, Left err) -> Left err
+      (Right (ValBool _), Right (ValInt _)) -> Left "Error: type mismatch in Eql"
+      (Right (ValInt _), Right (ValBool _)) -> Left "Error: type mismatch in Eql"
+      (Right (ValBool x), Right (ValBool y)) -> Right (ValBool (x == y))
+      (Right (ValInt x), Right (ValInt y)) -> Right (ValBool (x == y))
+  If e1 e2 e3 ->
+      case (eval e1) of
+        (Left err) -> Left err
+        Right (ValInt _) -> Left "Error: if statement evaluates to integer"
+        Right (ValBool b) ->
+          if b then
+            (eval e2)
+          else
+            (eval e3)
--- week1/handout/src/APL/Eval_Tests.hs
+++ week1/handout/src/APL/Eval_Tests.hs
@@ -1,12 +1,37 @@
 module APL.Eval_Tests (tests) where
 
-import APL.AST ()
-import APL.Eval ()
+import APL.AST (Exp (..))
+import APL.Eval (Val (..), eval)
 import Test.Tasty (TestTree, testGroup)
-import Test.Tasty.HUnit ()
+import Test.Tasty.HUnit (testCase, (@?=))
 
 tests :: TestTree
 tests =
   testGroup
     "Evaluation"
-    []
+    [test1, test2, test3, test4, test5, test6, test7, test8]
+
+test1 :: TestTree
+test1 = testCase "Integer test" $ eval (CstInt 10) @?= Right (ValInt 10)
+
+test2 :: TestTree
+test2 = testCase "Addition test" $ eval (Add (CstInt 10) (CstInt 18)) @?= Right (ValInt 28)
+
+test3 :: TestTree
+test3 = testCase "Subtraction test" $ eval (Sub (CstInt 10) (CstInt 4)) @?= Right (ValInt 6)
+
+test4 :: TestTree
+test4 = testCase "Multiplication test" $ eval (Mul (CstInt 10) (CstInt 18)) @?= Right (ValInt 180)
+
+test5 :: TestTree
+test5 = testCase "Division test" $ eval (Div (CstInt 18) (CstInt 9)) @?= Right (ValInt 2)
+
+test6 :: TestTree
+test6 = testCase "Power test" $ eval (Pow (CstInt 10) (CstInt 2)) @?= Right (ValInt 100)
+
+test7 :: TestTree
+test7 = testCase "Division test fail" $ eval (Div (CstInt 10) (CstInt 0)) @?= Left "Error: division by zero"
+
+test8 :: TestTree
+test8 = testCase "Division power fail" $ eval (Pow (CstInt 10) (CstInt (-8))) @?= Left "Error: negative exponent"
+
