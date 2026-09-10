ed1d27f4da4d08669a7eac01a9ee2627ef6138ca
nygaardlarsen <mathias.nygaard.iversen@gmail.com>
2026-09-10 19:08:27 +0200

    packed files in tar archive for a1

---
+++ a1/a1-code/src/APL/AST.hs
@@ -0,0 +1,48 @@
+module APL.AST
+  ( VName,
+    Exp (..),
+    printExp,
+  )
+where
+
+type VName = String
+
+data Exp
+  = CstInt Integer
+  | CstBool Bool
+  | Add Exp Exp
+  | Sub Exp Exp
+  | Mul Exp Exp
+  | Div Exp Exp
+  | Pow Exp Exp
+  | Eql Exp Exp
+  | If Exp Exp Exp
+  | Var VName
+  | Let VName Exp Exp
+  | ForLoop (VName, Exp) (VName, Exp) Exp
+  | Lambda VName Exp
+  | Apply Exp Exp
+  | TryCatch Exp Exp
+  -- TODO: add cases
+  deriving (Eq, Show)
+
+printExp :: Exp -> String
+printExp (CstInt x) = show x
+printExp (CstBool True) = "true"
+printExp (CstBool False) = "false"
+printExp (Var v) = v
+printExp (Add e1 e2) = "(" ++ printExp e1 ++ " + " ++ printExp e2 ++ ")"
+printExp (Sub e1 e2) = "(" ++ printExp e1 ++ " - " ++ printExp e2 ++ ")"
+printExp (Mul e1 e2) = "(" ++ printExp e1 ++ " * " ++ printExp e2 ++ ")"
+printExp (Div e1 e2) = "(" ++ printExp e1 ++ " / " ++ printExp e2 ++ ")"
+printExp (Pow e1 e2) = "(" ++ printExp e1 ++ " ** " ++ printExp e2 ++ ")"
+printExp (Eql e1 e2) = "(" ++ printExp e1 ++ " == " ++ printExp e2 ++ ")"
+printExp (If e1 e2 e3) = "(if " ++ printExp e1 ++ " then " ++ printExp e2 ++ " else " ++ printExp e3 ++ ")"
+printExp (Let v e1 e2) = "(let " ++ v ++ " = " ++ printExp e1 ++ " in " ++ printExp e2 ++ ")"
+printExp (ForLoop (v1, e1) (v2, e2) e3) = "(loop " ++ v1 ++ " = " ++ printExp e1 ++ " for " ++ v2 ++ " < " ++ printExp e2 ++ " do " ++ printExp e3 ++ ")"
+printExp (Lambda v e) = "(\\" ++ v ++ " -> " ++ printExp e ++ ")"
+printExp (Apply e1 e2) = "("  ++ printExp e1 ++ " " ++ printExp e2 ++ ")"
+printExp (TryCatch e1 e2) = "(try "  ++ printExp e1 ++ " catch " ++ printExp e2 ++ ")"
+
+
+
