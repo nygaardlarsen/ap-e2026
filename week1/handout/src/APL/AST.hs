6c74d33f3b44d97bc6225f22e9f011c3a2064a7e
nygaardlarsen <mathias.nygaard.iversen@gmail.com>
2026-09-10 11:12:24 +0200

    first changes to a1

--- week1/handout/src/APL/AST.hs
+++ week1/handout/src/APL/AST.hs
@@ -1,4 +1,17 @@
 module APL.AST
   (
+    Exp(..)
   )
 where
+
+data Exp
+  = CstInt Integer
+  | Add Exp Exp
+  | Sub Exp Exp
+  | Mul Exp Exp
+  | Div Exp Exp
+  | Pow Exp Exp
+  | CstBool Bool
+  | Eql Exp Exp
+  | If Exp Exp Exp
+  deriving (Eq, Show)
