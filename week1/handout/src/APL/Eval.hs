2bd2fa615e4b776871779607c846cd30f344fcb9
nygaardlarsen <mathias.nygaard.iversen@gmail.com>
2026-09-10 12:05:55 +0200

    finished exercises from week1

--- week1/handout/src/APL/Eval.hs
+++ week1/handout/src/APL/Eval.hs
@@ -1,44 +1,59 @@
 module APL.Eval
   (
-    Val(..), eval
+    Val(..), eval, envEmpty, envExtend, envLookup
   )
 where
 
-import APL.AST (Exp(..))
+import APL.AST (Exp(..), VName) 
 
 type Error = String
+
+type Env = [(VName, Val)]
+
+envEmpty :: Env
+envEmpty = []
+
+-- | Extend an environment with a new variable binding,
+-- producing a new environment.
+envExtend :: VName -> Val -> Env -> Env
+envExtend n v vtable = (n, v) : vtable
+
+-- | Look up a variable name in the provided environment.
+-- Returns Nothing if the variable is not in the environment.
+envLookup :: VName -> Env -> Maybe Val
+envLookup n vtable = lookup n vtable
 
 data Val
   = ValInt Integer
   | ValBool Bool
   deriving (Eq, Show)
 
-eval :: Exp -> Either Error Val
-eval e = case e of
+eval :: Env -> Exp -> Either Error Val
+eval env e = case e of
   (CstInt x) -> Right (ValInt x)
   Add e1 e2 -> 
-    case (eval e1, eval e2) of 
+    case (eval env e1, eval env e2) of 
       (Left err, _) -> Left err
       (_, Left err) -> Left err
       (Right (ValInt x), Right (ValInt y)) -> Right (ValInt (x + y))
   Sub e1 e2 ->
-    case (eval e1, eval e2) of 
+    case (eval env e1, eval env e2) of 
       (Left err, _) -> Left err
       (_, Left err) -> Left err
       (Right (ValInt x), Right (ValInt y)) -> Right (ValInt (x - y))
   Mul e1 e2 ->
-    case (eval e1, eval e2) of 
+    case (eval env e1, eval env e2) of 
       (Left err, _) -> Left err
       (_, Left err) -> Left err
       (Right (ValInt x), Right (ValInt y)) -> Right (ValInt (x * y))
   Div e1 e2 ->
-    case (eval e1, eval e2) of 
+    case (eval env e1, eval env e2) of 
       (Left err, _) -> Left err
       (_, Left err) -> Left err
       (_, Right (ValInt 0)) -> Left "Error: division by zero"
       (Right (ValInt x), Right (ValInt y)) -> Right (ValInt (div x y))
   Pow e1 e2 -> 
-    case (eval e1, eval e2) of 
+    case (eval env e1, eval env e2) of 
       (Left err, _) -> Left err
       (_, Left err) -> Left err
       (Right (ValInt x), Right (ValInt y)) -> 
@@ -46,7 +61,7 @@ eval e = case e of
         else Right (ValInt (x ^ y))
   (CstBool b) -> Right (ValBool b)
   Eql e1 e2 ->
-    case (eval e1, eval e2) of
+    case (eval env e1, eval env e2) of
       (Left err, _) -> Left err
       (_, Left err) -> Left err
       (Right (ValBool _), Right (ValInt _)) -> Left "Error: type mismatch in Eql"
@@ -54,11 +69,19 @@ eval e = case e of
       (Right (ValBool x), Right (ValBool y)) -> Right (ValBool (x == y))
       (Right (ValInt x), Right (ValInt y)) -> Right (ValBool (x == y))
   If e1 e2 e3 ->
-      case (eval e1) of
+      case (eval env e1) of
         (Left err) -> Left err
         Right (ValInt _) -> Left "Error: if statement evaluates to integer"
         Right (ValBool b) ->
           if b then
-            (eval e2)
+            (eval env e2)
           else
-            (eval e3)
+            (eval env e3)
+  Var v ->
+    case (envLookup v env) of
+      (Just x) -> Right x
+      Nothing -> Left $ "Error: Unknown variable " ++ v
+  Let var e1 e2 -> 
+    case (eval env e1) of
+      (Left err) -> Left err
+      (Right v) -> eval (envExtend var v env) e2
--- week1/handout/src/APL/Eval_Tests.hs
+++ week1/handout/src/APL/Eval_Tests.hs
@@ -1,7 +1,7 @@
 module APL.Eval_Tests (tests) where
 
 import APL.AST (Exp (..))
-import APL.Eval (Val (..), eval)
+import APL.Eval (Val (..), eval, envEmpty)
 import Test.Tasty (TestTree, testGroup)
 import Test.Tasty.HUnit (testCase, (@?=))
 
@@ -9,29 +9,36 @@ tests :: TestTree
 tests =
   testGroup
     "Evaluation"
-    [test1, test2, test3, test4, test5, test6, test7, test8]
+    [test1, test2, test3, test4, test5, test6, test7, test8, test9, test10]
 
 test1 :: TestTree
-test1 = testCase "Integer test" $ eval (CstInt 10) @?= Right (ValInt 10)
+test1 = testCase "Integer test" $ eval envEmpty (CstInt 10) @?= Right (ValInt 10)
 
 test2 :: TestTree
-test2 = testCase "Addition test" $ eval (Add (CstInt 10) (CstInt 18)) @?= Right (ValInt 28)
+test2 = testCase "Addition test" $ eval envEmpty (Add (CstInt 10) (CstInt 18)) @?= Right (ValInt 28)
 
 test3 :: TestTree
-test3 = testCase "Subtraction test" $ eval (Sub (CstInt 10) (CstInt 4)) @?= Right (ValInt 6)
+test3 = testCase "Subtraction test" $ eval envEmpty (Sub (CstInt 10) (CstInt 4)) @?= Right (ValInt 6)
 
 test4 :: TestTree
-test4 = testCase "Multiplication test" $ eval (Mul (CstInt 10) (CstInt 18)) @?= Right (ValInt 180)
+test4 = testCase "Multiplication test" $ eval envEmpty (Mul (CstInt 10) (CstInt 18)) @?= Right (ValInt 180)
 
 test5 :: TestTree
-test5 = testCase "Division test" $ eval (Div (CstInt 18) (CstInt 9)) @?= Right (ValInt 2)
+test5 = testCase "Division test" $ eval envEmpty (Div (CstInt 18) (CstInt 9)) @?= Right (ValInt 2)
 
 test6 :: TestTree
-test6 = testCase "Power test" $ eval (Pow (CstInt 10) (CstInt 2)) @?= Right (ValInt 100)
+test6 = testCase "Power test" $ eval envEmpty (Pow (CstInt 10) (CstInt 2)) @?= Right (ValInt 100)
 
 test7 :: TestTree
-test7 = testCase "Division test fail" $ eval (Div (CstInt 10) (CstInt 0)) @?= Left "Error: division by zero"
+test7 = testCase "Division test fail" $ eval envEmpty (Div (CstInt 10) (CstInt 0)) @?= Left "Error: division by zero"
 
 test8 :: TestTree
-test8 = testCase "Division power fail" $ eval (Pow (CstInt 10) (CstInt (-8))) @?= Left "Error: negative exponent"
+test8 = testCase "Division power fail" $ eval envEmpty (Pow (CstInt 10) (CstInt (-8))) @?= Left "Error: negative exponent"
+
+test9 :: TestTree
+test9 = testCase "Division power fail" $ eval envEmpty (Let "x" (CstInt 5) (Var "x")) @?= Right (ValInt 5)
+
+test10 :: TestTree
+test10 = testCase "Division power fail" $ eval envEmpty (Var "x") @?= Left "Error: Unknown variable x"
+
 
